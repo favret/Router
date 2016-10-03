3df157409d6aedf454cca46c1698a3fa8b99b6b8
favre <save-favret@MacBook-Pro-de-favre-2.local>
2016-10-03 12:12:07 +0200

    feat(master): add carthage build

---
+++ Carthage/Build/iOS/Router.framework/Headers/Router.h
@@ -0,0 +1,19 @@
+//
+//  Router.h
+//  Router
+//
+//  Created by favre on 29/09/2016.
+//  Copyright © 2016 favre. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Router.
+FOUNDATION_EXPORT double RouterVersionNumber;
+
+//! Project version string for Router.
+FOUNDATION_EXPORT const unsigned char RouterVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Router/PublicHeader.h>
+
+
